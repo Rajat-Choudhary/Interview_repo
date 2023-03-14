53b3c2489ee4fa4c9a0dbbc33f2f1c4237f848d5
Rajat <rajatchoudhary1890@gmail.com>
2023-03-14 09:00:23 +0530

    Mar 14 String next 6 question

--- string/solutions.py
+++ string/solutions.py
@@ -55,3 +55,56 @@
 # print(fun("abc"))
 # print(fun("string"))
 
+"""Solution 8"""
+# sample_string = "php python Java"
+# convert_into_list = sample_string.split(" ")
+# result = max(convert_into_list,key=len)
+# count = 0
+# for i in result:
+#     count +=1
+# print(f'Longest Word is:-{result}')
+# print(f'Number of Letter is:-{count}')
+
+"""Solution 9"""
+# sample_string = "python"
+# n = int(input("Enter the index of removing char:"))
+# if len(sample_string) >= n:
+#     result = sample_string.replace(sample_string[n],'')
+#     print(result)
+# else:
+#     print("Index Out of Range")
+
+"""Solution 10"""
+# sample_string = "abcd"
+# first_char = sample_string[-1:]
+# print(first_char)
+# last_char = sample_string[:1]
+# print(last_char)
+# result = last_char + sample_string[1:-1] + first_char
+# print(result)
+
+"""Solution 11"""
+# sample_string = "Rajatchoudhary"
+# result = ''
+# for i in range(len(sample_string)):
+#     if i % 2 == 0:
+#         result = result + sample_string[i]
+# print(result)
+
+"""Solution 12"""
+# sample_string = "the quick brown fox jumps over the lazy dog."
+# words = sample_string.split(" ")
+# d = dict()
+# for word in words:
+#     if word in d:
+#         d[word] += 1
+#     else:
+#         d[word] = 1
+# print(d)
+
+
+"""Solution 13"""
+# sample_string = "the quick brown fox jumps over the lazy dog."
+# sample_string2 = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+# print(sample_string.upper())
+# print(sample_string2.lower())
